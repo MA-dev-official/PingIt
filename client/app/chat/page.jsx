2a3d153dcd25c0e7d516e5c6d220fa732805433e
buraqgamer8 <40659371-buraqgamer8@users.noreply.replit.com>
2025-07-17 20:18:14 +0000

    fixing front-end

--- client/app/chat/page.jsx
+++ client/app/chat/page.jsx
@@ -1,6 +1,12 @@
 // /app/chat/page.jsx
+
+import { Suspense } from "react";
 import ChatClient from "@/components/ChatClient";
 
-export default function Page() {
-  return <ChatClient />;
+export default function ChatPage() {
+  return (
+    <Suspense fallback={<div>Loading chat...</div>}>
+      <ChatClient />
+    </Suspense>
+  );
 }
